f2c6a4d0c6c17a712134441c23ae07ea0aaa3105
Sneha Barman <sneha.barman2004@gmail.com>
2025-09-19 17:21:39 +0530

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -42,7 +42,7 @@ function App() {
   return (
     <div style={{ display: "flex", height: "100vh", width: "100%" }}>
       {/* Left: Visualization */}
-      <div style={{ flex: 1, padding: 10, width: "150%" }}>
+      <div style={{ flex: 1, padding: 10, width: "100%" }}>
         <h2>Visualization</h2>
         {currentVis ? (
           <VisualizationCanvas visualization={currentVis} />
